65557c14beaa08e349ee9a2a5c2f8ca68fa13b7e
Lucas <lucasbolelasilveira@gmail.com>
2022-03-23 11:59:49 -0300

    Atualizações do botão +

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const fetchData = async url => {
     return await response.json()
 }
 
-const trasportadoras = document.getElementById('transportadora');
-const modelos = document.getElementById('modeloPersiana');
-const tecidos = document.getElementById('tecidoPersiana');
+fetchData('./api.json').then(res => {
+    let trasportadoras = document.getElementById('transportadora');
+    res.transportadoras.forEach((transportadora,i) => {
+        trasportadoras.innerHTML += `
+            <option value="${i}">${transportadora}</option>
+        `;
+    });
+})
 
-json = fetchData('./api.json')
+const containerCard = document.getElementById('cardContainer');
+
+let quantidadeProduto = 0;
+
+const json = fetchData('./api.json')
     .then(res => {
-        res.transportadoras.forEach((transportadora,i) => {
-            trasportadoras.innerHTML += `
-                <option value="${i}">${transportadora}</option>
-            `;
-        });
+        let modelos = document.getElementById('modeloPersiana'+ quantidadeProduto);
+        let tecidos = document.getElementById('tecidoPersiana'+ quantidadeProduto);
 
         res.modelos.forEach((modelo) => {
             modelos.innerHTML += `
@@ -26,7 +32,6 @@ json = fetchData('./api.json')
             tecidos.innerHTML = '<option selected>Escolha o tecido...</option>';
             
             for ( let tipo in res.tipos) {
-                console.log(tipo)
                 if (tipo == modeloAtual) {
                     res.tipos[tipo].forEach((tipo) => {
                         tecidos.innerHTML += `
@@ -37,7 +42,8 @@ json = fetchData('./api.json')
                     break;
                 }
             }
-            if (tecidos.childElementCount == 0) {
+
+            if (tecidos.childElementCount == 1) {
                 tecidos.disabled = true;
             } 
         });
@@ -51,5 +57,43 @@ const calculaPersiana = ( modeloPersiana, tecidoPersiana, qtde, largura, altura,
 }
 
 const adicionarNovaPersiana = () => {
+    quantidadeProduto++;
 
+    containerCard.innerHTML += `
+        <div id="card${quantidadeProduto}" class="col">
+            <div class="card rounded-3 shadow-sm">
+                <div class="card-header py-3">
+                    <h4 class="my-0 fw-normal">Persiana <span id="idPersiana${quantidadeProduto}">${quantidadeProduto}</span></h4>
+                </div>
+                <div class="card-body row g-2">
+                    <div class="col-sm-6">
+                        <label class="visually-hidden" for="modeloPersiana">Modelo Persiana</label>
+                        <select class="form-select" id="modeloPersiana${quantidadeProduto}">
+                            <option selected="">Escolha o modelo...</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-6">
+                        <label class="visually-hidden" for="tecidoPersiana">Tecido Persiana</label>
+                        <select class="form-select" id="tecidoPersiana${quantidadeProduto}" disabled=""><option selected="">Escolha o tecido...</option></select>
+                    </div>
+                    <div class="col-sm-4">
+                        <label class="visually-hidden" for="quantidade">Quantidade por peça</label>
+                        <input type="number" class="form-control" id="quantidade${quantidadeProduto}" placeholder="Quantidade por peça">
+                    </div>
+                    <div class="col-sm-4">
+                        <label class="visually-hidden" for="largura">Largura por peça</label>
+                        <input type="number" class="form-control" id="largura${quantidadeProduto}" placeholder="Largura por peça">
+                    </div>
+                    <div class="col-sm-4">
+                        <label class="visually-hidden" for="altura">Altura por peça</label>
+                        <input type="number" class="form-control" id="altura${quantidadeProduto}" placeholder="Altura por peça">
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    json;
 }
+
+adicionarNovaPersiana();
